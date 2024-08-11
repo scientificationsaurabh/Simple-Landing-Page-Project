a9eb5b4e50a379d515aedf79b9f2f727cea885fb
RADHANPARA SAURABH ASHOKBHAI <137665901+scientificationsaurabh@users.noreply.github.com>
2024-08-11 09:05:08 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.shop-now').addEventListener('click', function () {
+        document.getElementById('shop').scrollIntoView({ behavior: 'smooth' });
+    });
+});
